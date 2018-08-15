e939910d8a43236dec41993e3a3ebecc7d3ffc89
woaitang131426 <87383325@qq.com>
2018-08-15 17:48:05 +1200

    add chasingEnemy

---
+++ Assets/Scripts/chasingEnemy.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class chasingEnemy : MonoBehaviour {
+
+    public Transform target;
+
+    public Transform myTransform;
+
+    private void Update()
+    {
+        transform.LookAt(target);
+        transform.Translate(Vector3.forward * 5 * Time.deltaTime);
+    }
+
+}
